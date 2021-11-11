74f5e2a239adbf344793fea46731c54cdd5af4a5
nguyenthanhdoan <donald.nguyen.it@gmail.com>
2021-11-11 12:38:17 +0700

    refactory

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'src'
+NAME = 'cnbcfinance'
 DESCRIPTION = 'The package fetch stock data from cnbc.'
 URL = 'https://github.com/dearvn/cnbcfinance'
 EMAIL = 'donald.nguyen.it@gmail.com'
--- tests/test-quote.py
+++ tests/test-quote.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from src.cnbcfinance import get_quota
+from cnbcfinance.cnbcfinance import get_quota
 
 if __name__ == '__main__':
     data = get_quota('AAPL')
