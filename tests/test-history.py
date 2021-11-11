e3b559a4f0f86968c98a0c65035addc27a748af8
nguyenthanhdoan <donald.nguyen.it@gmail.com>
2021-11-11 12:24:14 +0700

    refactory

--- tests/test-history.py
+++ tests/test-history.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from cnbcfinance.cnbcfinance import get_history_df
+from src.cnbcfinance import get_history_df
 
 if __name__ == '__main__':
     data = get_history_df('AAPL', '30m')
